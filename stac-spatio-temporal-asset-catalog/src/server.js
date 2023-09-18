048727b7a06df1fd7a1448a20f2ceef78b29ebaf
michael_b <m.brueggemann7@web.de>
2023-09-18 15:55:01 +0200

    Routen zum STAC ergänzt

--- stac-spatio-temporal-asset-catalog/src/server.js
+++ stac-spatio-temporal-asset-catalog/src/server.js
@@ -1,20 +1,49 @@
 const EXPRESS = require('express')
 const APP = EXPRESS()
+const PATH = require("path")
 let  landing_page = require("./stac_example/catalog.json")
+let gebaeude_in_muenster = require("./stac_example/gebaeude-in-muenster/collection.json")
+let item1 = require("./stac_example/gebaeude-in-muenster/1.json")
+let item2 = require("./stac_example/gebaeude-in-muenster/2.json")
 
 const host = 'localhost'
 const port = '3000'
 
+// landing page
 APP.get('/', function(_req, res) {
     res.json(landing_page)
-});
+})
 
+// service-desc
+APP.get("/api", function(_req, res) {
+    res.json({api_name: "stac example API", api_feature1: "Auflistung der Features dieser API", etc: ""})
+})
+
+// service-doc
+APP.get("/api.html", function(_req, res) {
+    res.sendFile(PATH.join(__dirname, "/api.html"))
+})
+
+// child "gebäude-in-münster". Usually would be programmed to give dynamic access to all collections, but for this example its programmed statically.
+APP.get("/gebaeude-in-muenster", function(_req, res) {
+    res.json(gebaeude_in_muenster)
+})
+
+// item "1"
+APP.get("/gebaeude-in-muenster/1", function(_req, res) {
+    res.json(item1)
+})
+
+// item "2"
+APP.get("/gebaeude-in-muenster/2", function(_req, res) {
+    res.json(item2)
+})
 
 // Explicitly tell the client that there is no favicon
 APP.get('/favicon.ico', (req, res) => res.status(204))
 
 // APP.use(EXPRESS.static('static'))
 
-console.log(`http://${host}:${port}`)
+console.log(`Server is running on: http://${host}:${port}`)
 
 APP.listen(port, host)
